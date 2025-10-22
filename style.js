a49f9cb7e5ff39de52e7dc69b437cb19451deb8f
dishitaa26 <dishitaapbsccse25@rvu.edu.in>
2025-10-22 22:01:03 +0530

    Add files via upload

---
+++ style.js
@@ -0,0 +1,8 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Welcome to KINDHOPE  - A website for kindness and hope!");
+});
+
+function showMessage() {
+  alert("Thank you for spreading kindness ");
+}
